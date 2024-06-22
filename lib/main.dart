c63b9802f4a5fefc21160f5a95e0a01fe6d44c3e
Hichri-Mohamed-Yassine <hichriy881@gmail.com>
2024-06-22 09:29:20 +0100

    adding all images with their questions

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:collection';
+
 import 'package:flutter/material.dart';
+import 'question.dart';
 
 void main() {
   runApp(const ExamApp());
@@ -36,21 +39,91 @@ class ExamPage extends StatefulWidget {
 }
 
 class _ExamPageState extends State<ExamPage> {
+  List<Widget> answerResult = [];
+  void checkAnswer(bool whatUserPicked) {
+    bool? correctAnswer = questionGroup[questionNumber].questionAnswer;
+    setState(() {
+      if (whatUserPicked == correctAnswer) {
+        answerResult.add(Padding(
+          padding: const EdgeInsets.all(3.0),
+          child: Icon(
+            Icons.thumb_up,
+            color: Colors.green,
+          ),
+        ));
+      } else {
+        answerResult.add(Padding(
+          padding: const EdgeInsets.all(3.0),
+          child: Icon(
+            Icons.thumb_down,
+            color: Colors.red,
+          ),
+        ));
+      }
+
+      questionNumber++;
+    });
+  }
+
+  /*List<String> questions = [
+    "The number of planets in the solar system is eight",
+    "cats is Carnivorous",
+    "China is located on the African continent",
+    "The Earth is flat, not spherical"
+  ];
+  List<String> questionImage = [
+    "images/image-1.jpg",
+    "images/image-2.jpg",
+    "images/image-3.jpg",
+    "images/image-4.jpg"
+  ];
+  List<bool> answers = [true, true, false, false];
+  */
+  List<Question> questionGroup = [
+    Question(
+        q: "The number of planets in the solar system is eight",
+        i: "images/image-1.jpg",
+        a: true),
+    Question(q: "cats is Carnivorous", i: "images/image-2.jpg", a: true),
+    Question(
+        q: "China is located on the African continent",
+        i: "images/image-3.jpg",
+        a: false),
+    Question(
+        q: "The Earth is flat, not spherical",
+        i: "images/image-4.jpg",
+        a: false),
+    Question(
+        q: "Humans can survive without eating meat",
+        i: "images/image-5.jpg",
+        a: true),
+    Question(
+        q: "The sun revolves around the earth and the earth revolves around the moon",
+        i: "images/image-6.jpg",
+        a: false),
+    Question(q: "Animals do not feel pain", i: "images/image-7.jpg", a: false),
+  ];
+
+  int questionNumber = 0;
   @override
   Widget build(BuildContext context) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
+        Row(
+          children: answerResult,
+        ),
         Expanded(
           flex: 5,
           child: Column(
             children: [
-              Image.asset("images/image-1.jpg"),
+              Image.asset(
+                  questionGroup[questionNumber].questionImage.toString()),
               const SizedBox(
                 height: 20.0,
               ),
               Text(
-                "The number of planets in the solar system is eight",
+                questionGroup[questionNumber].questionText.toString(),
                 textAlign: TextAlign.center,
                 style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
               )
@@ -63,7 +136,9 @@ class _ExamPageState extends State<ExamPage> {
             child: ElevatedButton(
                 style: ElevatedButton.styleFrom(
                     backgroundColor: Colors.indigoAccent),
-                onPressed: () {},
+                onPressed: () {
+                  checkAnswer(true);
+                },
                 child: const Text(
                   "True",
                   style: TextStyle(fontSize: 22.0, color: Colors.white),
@@ -75,7 +150,9 @@ class _ExamPageState extends State<ExamPage> {
             padding: const EdgeInsets.symmetric(vertical: 10.0),
             child: ElevatedButton(
                 style: ElevatedButton.styleFrom(backgroundColor: Colors.red),
-                onPressed: () {},
+                onPressed: () {
+                  checkAnswer(false);
+                },
                 child: const Text(
                   "False",
                   style: TextStyle(fontSize: 22.0, color: Colors.white),
